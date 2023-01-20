358c10db54e722c3e7412e7e90d831f9eb2ee32d
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2023-01-20 15:09:06 -0300

    ativa o serviço do apache

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 yum update -y
 yum install -y git httpd
+systemctl enable --now httpd
 cd /root/
 git clone https://github.com/jofernando/compass-pb-atv-2.git
 cp compass-pb-atv-2/etc/crontab /etc/crontab
